a3f6536952964626b15df6d06efa00fbe3f05fc8
Jose Pablo Garbanzo <jose.garbanzo@huli.io>
2025-03-05 18:24:00 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,106 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  data() {
+    return {
+      message: 'Mensaje desde data',
+      contadorLikes: 10,
+    }
+  },
+
+  methods: {
+    incrementarLikes() {
+      this.contadorLikes = this.contadorLikes + 1
+    },
+  },
+
+  components: {
+    'hello-world': HelloWorld,
+  },
+}
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <hello-world v-bind:msg="message" v-bind:contador-likes="contadorLikes" />
+      <button v-on:click="incrementarLikes">Dar Like</button>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <a href="/about">About con tag de a</a>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
